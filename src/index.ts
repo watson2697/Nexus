89573838c66bf1954b6cda3ce319f5bd84c01b6f
watson2697 <gjoreskaklara97@gmail.com>
2023-06-17 00:02:47 +0200

    feat: initialized express server

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
-const _name = "ace"
+import express from "express"
 
-console.log(`Hello ${_name}`)
+const main = async () => {
+    const port = process.env.PORT || 7000
+
+    const app = express()
+    app.listen(port, () => console.log(`Server is running on port ${port}`))
+
+    app.get('/greet/:what', (req, res) => res.json({greeating:`Ciao a ${req.params.what}`}))
+
+}
+
+main()
